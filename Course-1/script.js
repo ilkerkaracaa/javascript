f672847d645d81524a66c1c70ba330259171d106
İlker Karaca <ikaracadeveloper@gmail.com>
2023-05-24 00:30:29 +0300

    javascript classes

--- Course-1/script.js
+++ Course-1/script.js
@@ -1,4 +1,30 @@
-let js = "amazing";
+class hero {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-let a = 2 + 3 + 4 - 5;
-console.log(a);
+  getAge() {
+    return this.age;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setAge(age) {
+    this.age = age;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+}
+
+hero1 = new hero("Batman", 30);
+console.log(hero1.getName());
+console.log(hero1.getAge());
+hero1.setName("Superman");
+hero1.setAge(40);
+console.log(hero1.getName());
+console.log(hero1.getAge());
