8a0a26c10d3c4d73fb6129743fa2bc363bb901f4
İlker Karaca <ikaracadeveloper@gmail.com>
2023-06-17 18:40:18 +0300

    classes

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+class deneme {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  getAge() {
+    console.log(this.age);
+  }
+
+  getName() {
+    console.log(this.name);
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  setAge(age) {
+    this.age = age;
+  }
+}
