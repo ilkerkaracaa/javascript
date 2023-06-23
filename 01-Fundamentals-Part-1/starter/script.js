90bd2f92a5a1920fbc90cfb6be0225d3747d321c
İlker Karaca <ikaracadeveloper@gmail.com>
2023-06-23 19:12:31 +0300

    let, const and var completed

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -59,3 +59,16 @@ console.log(country);
 console.log(language);
 
 */
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Karaca";
+console.log(lastName);
