76cecc7ec32b243deee2311ce320ec49c0489980
İlker Karaca <ikaracadeveloper@gmail.com>
2023-06-23 17:49:29 +0300

    Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,22 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "İlker";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let simple = "Simple";
+let $function = 27;
+
+let PI = 3.1415;
+let person = "İlker";
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
