c388cb70dbe878f871a45f850a641277ae8697c8
İlker Karaca <ikaracadeveloper@gmail.com>
2023-06-25 02:24:29 +0300

    Truthy and Falsy Values completed

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -184,3 +184,24 @@ console.log(2 + 3 + 4 + "5");
 
 console.log("10" - "4" - "3" - 2 + "5");
 */
+/*
+console.log(0);
+console.log(Boolean(undefined));
+console.log(Boolean(0));
+console.log(Boolean("İlker"));
+console.log(Boolean({}));
+
+const money = 0;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height = 0;
+if (height) {
+  console.log("YAY! Height is defined");
+} else {
+  console.log("Height is UNDEFINED");
+}
+*/
