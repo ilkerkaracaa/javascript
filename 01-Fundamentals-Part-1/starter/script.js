54f482f064143aabbce03b82cbdd6186e527bc1d
İlker Karaca <ikaracadeveloper@gmail.com>
2023-06-25 01:41:02 +0300

    Type Conversion and Coercion completed

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -159,3 +159,28 @@ if (birthYear <= 2000) {
 }
 console.log(century);
 */
+/*
+//type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("İlker"));
+console.log(typeof NaN);
+
+console.log(String(24), 24);
+
+// Type coercion
+console.log("I am " + 24 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" * "2");
+console.log("23" > "18");
+
+let n = "1" + 1; // '11'
+n = n - 1;
+console.log(n);
+
+console.log(2 + 3 + 4 + "5");
+
+console.log("10" - "4" - "3" - 2 + "5");
+*/
