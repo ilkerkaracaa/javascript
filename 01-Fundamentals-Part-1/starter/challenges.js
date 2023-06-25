ac87cc935a3c1801b335c6b18abab36ba0c1bd56
İlker Karaca <ikaracadeveloper@gmail.com>
2023-06-25 03:35:46 +0300

    Challenge completed

--- 01-Fundamentals-Part-1/starter/challenges.js
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -44,3 +44,13 @@ if (population > averagePopulation) {
     } million below average`
   );
 }
+const numNeighbours = Number(
+  prompt("How many neighbour countries does your country have?")
+);
+if (numNeighbours === 1) {
+  console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
