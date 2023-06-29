3f6913e7f80669f2b4ce8fbeb4ef615230a0d2d5
İlker Karaca <ikaracadeveloper@gmail.com>
2023-06-29 16:51:36 +0300

    Functions are a type of value

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -7,3 +7,26 @@ const passTest = true;
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("I can drive :D");
 */
+/*
+function logger() {
+  console.log("My name is İlker");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+*/
