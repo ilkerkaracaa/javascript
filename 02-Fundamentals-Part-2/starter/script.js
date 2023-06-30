d201d41d9ac165cb6702aa74e52ac251bfc61bd7
İlker Karaca <ikaracadeveloper@gmail.com>
2023-06-30 10:44:31 +0300

    updated script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -45,6 +45,7 @@ const age2 = calcAge2(1991);
 
 console.log(age1, age2);
 */
+/*
 const calcAge = (birthYear) => 2023 - birthYear;
 const age = calcAge(1999);
 console.log(age);
@@ -58,3 +59,4 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1999, "İlker"));
 console.log(yearsUntilRetirement(1980, "Bob"));
+*/
