afad4e9bb61f0aae640618381d88cefdb14631fd
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-01 17:38:11 +0300

    Functions Calling Other Functions section completed

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -60,3 +60,17 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log(yearsUntilRetirement(1999, "İlker"));
 console.log(yearsUntilRetirement(1980, "Bob"));
 */
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
