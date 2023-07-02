decfab8dfe5c4537bda621af55cc36074d6ba12f
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-02 22:20:41 +0300

    Looping Backward and Loops in Loops is completed.

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -315,3 +315,27 @@ for (let i = 0; i < ilkerArray.length; i++) {
   console.log(ilkerArray[i], typeof ilkerArray[i]);
 }
 */
+/*
+const ilkerArray = [
+  "İlker",
+  "Karaca",
+  2023 - 1999,
+  "student",
+  ["Michael", "Peter", "Steven"],
+];
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let i = ilkerArray.length - 1; i >= 0; i--) {
+  console.log(i, ilkerArray[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`----- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+  }
+}
+*/
