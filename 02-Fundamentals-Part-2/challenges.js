bd357498d34d4fbd7cb892353e017473a893c58d
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-02 20:13:37 +0300

    challenge completed

--- 02-Fundamentals-Part-2/challenges.js
+++ 02-Fundamentals-Part-2/challenges.js
@@ -87,13 +87,19 @@ const myCountry = {
   language: "Turkish",
   population: 83,
   neighbours: ["Greece", "Bulgaria", "Georgia"],
+  describe: function () {
+    console.log(
+      `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`
+    );
+  },
+  checkIsland: function () {
+    this.isIsland = this.neighbours.length === 0 ? true : false;
+    return this.isIsland;
+  },
 };
 
-console.log(myCountry);
-
-console.log(
-  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
-);
+myCountry.describe();
+console.log(myCountry.checkIsland());
 myCountry.population += 2;
 console.log(myCountry.population);
 myCountry["population"] -= 2;
