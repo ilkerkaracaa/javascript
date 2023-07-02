41001fd3105620ad6b4926c1897e6877ffd457f5
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-02 20:21:17 +0300

    challenge is completed

--- 02-Fundamentals-Part-2/challenges.js
+++ 02-Fundamentals-Part-2/challenges.js
@@ -105,3 +105,6 @@ console.log(myCountry.population);
 myCountry["population"] -= 2;
 console.log(myCountry.population);
 */
+for (let i = 1; i <= 50; i++) {
+  console.log(`Voter number ${i} is currently voting`);
+}
