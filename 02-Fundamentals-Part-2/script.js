c0bdc71f401759e1a5469bbe19c35a7d656ce91e
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-02 22:46:26 +0300

    updated

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -339,7 +339,7 @@ for (let exercise = 1; exercise < 4; exercise++) {
   }
 }
 */
-
+/*
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weights repetition ${rep}`);
 }
@@ -358,3 +358,4 @@ while (dice !== 6) {
   dice = Math.trunc(Math.random() * 6) + 1;
   if (dice === 6) console.log("Loop is about to end...");
 }
+*/
