31c6b68302e332c1b5d4c64858063444f8dafbaa
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-04 18:46:36 +0300

    challange completed

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,2 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+function printForecast(arr) {
+  let string = '';
+  for (let i = 0; i < arr.length; i++) {
+    string += `${arr[i]} C in ${i + 1} days ... `;
+  }
+  console.log('... ' + string);
+}
+const arr = [17, 21, 23];
+printForecast(arr);
