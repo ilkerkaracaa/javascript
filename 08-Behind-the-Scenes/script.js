0a9a5e4c866aad1388c33b426d92bcbed8e4c82a
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-09 18:32:40 +0300

    Hoisting and TDZ in Practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   const age = 2021 - birthYear;
 
@@ -31,3 +32,44 @@ function calcAge(birthYear) {
 
   return age;
 }
+*/
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'İlker';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
