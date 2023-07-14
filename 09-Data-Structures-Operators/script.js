8d4de36be44aff874fbd8ae895336143bc69aacf
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-14 07:08:49 +0300

    updated

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -61,8 +61,35 @@ const restaurant = {
   },
 };
 
-//const arr = [1, 2, ...[3, 4]];
+console.log('---- OR ----');
 
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'Jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+//const arr = [1, 2, ...[3, 4]];
 // const [a, b, ...others] = [1, 2, 3, 4, 5];
 // console.log(a, b, others);
 
