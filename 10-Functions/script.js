c2b11529eb28bbb836669e50ff2f4279cfb2f078
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-17 10:11:34 +0300

    uptaded

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,38 @@
 'use strict';
 
+/*
+const flight = 'LH234';
+const ilker = {
+  name: 'Ilker Karaca',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+//checkIn(flight, ilker);
+//console.log(flight);
+//console.log(ilker);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = ilker;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(ilker);
+checkIn(flight, ilker);
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -26,3 +59,4 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+*/
