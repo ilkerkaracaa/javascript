d1c53366f55737071f8ccbd4fc800149bdde9f18
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-18 16:55:44 +0300

    uptaded

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -151,3 +151,23 @@ movements.forEach(function (mov, i, arr) {
 // 2: function(400)
 // ...
 */
+// forEach With Maps and Sets
+// Map
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+/*
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, i, set) {
+  console.log(`${value}: ${value}`);
+});
+*/
