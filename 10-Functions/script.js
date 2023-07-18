3b4ab7928b9fdb1c58b8359f70556c25d566697e
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-18 10:50:43 +0300

    uptaded

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,6 +1,30 @@
 'use strict';
 
 /*
+// Immediately Invoked Function Expressions (IIFE)
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
+
+/*
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
