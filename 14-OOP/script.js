6fd7b9bda67c8074a3ecceb091518c918e068a57
İlker Karaca <ikaracadeveloper@gmail.com>
2023-07-26 13:37:04 +0300

    modified

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -111,7 +111,7 @@ bmw.brake();
 
 // class expression
 // const PersonCl = class {}
-
+/*
 // class declaration
 class PersonCl {
   constructor(fullName, birthYear) {
@@ -190,3 +190,26 @@ account.latest = 50;
 console.log(account.movements);
 
 PersonCl.hey();
+*/
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
