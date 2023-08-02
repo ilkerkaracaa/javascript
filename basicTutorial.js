b75e0ff25d993b7a91a10daa9234933d1358e45d
İlker Karaca <ikaracadeveloper@gmail.com>
2023-08-02 18:28:11 +0300

    modified

---
+++ basicTutorial.js
@@ -0,0 +1,51 @@
+// const user = {};
+// user.name = "John";
+// user.surName = "Smith";
+// user.name = "Pete";
+// delete user.name;
+
+// function isEmpty(obj) {
+//   for (let key in obj) {
+//     name: "fasf";
+//     return false;
+//   }
+//   return true;
+// }
+
+// const anan = {};
+// console.log(isEmpty(anan));
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+
+// let sum = 0;
+
+// for (let key in salaries) {
+//   sum += salaries[key];
+// }
+
+// console.log(sum);
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+//   anan: "fafa",
+// };
+
+// console.log(salaries);
+
+// function multiplyNumeric(obj) {
+//   for (let key in obj) {
+//     if (typeof obj[key] === "number") {
+//       obj[key] *= 2;
+//     }
+//   }
+// }
+
+// multiplyNumeric(salaries);
+
+// console.log(salaries);
