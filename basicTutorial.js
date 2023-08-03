8bc6ba0b0aa6beed256ba764c4ee1a66dc274e81
İlker Karaca <ikaracadeveloper@gmail.com>
2023-08-03 19:14:28 +0300

    modified

--- basicTutorial.js
+++ basicTutorial.js
@@ -50,23 +50,42 @@
 
 // console.log(salaries);
 
-let calculator = {
-  a: 0,
-  b: 0,
-  read() {
-    this.a = +prompt("enter a number");
-    this.b = +prompt("enter a number");
-  },
+// let calculator = {
+//   a: 0,
+//   b: 0,
+//   read() {
+//     this.a = +prompt("enter a number");
+//     this.b = +prompt("enter a number");
+//   },
 
-  sum() {
-    return this.a + this.b;
-  },
+//   sum() {
+//     return this.a + this.b;
+//   },
 
-  mul() {
-    return this.a * this.b;
-  },
-};
+//   mul() {
+//     return this.a * this.b;
+//   },
+// };
 
-calculator.read();
-alert(calculator.sum());
-alert(calculator.mul());
+// calculator.read();
+// alert(calculator.sum());
+// alert(calculator.mul());
+
+// let ladder = {
+//   step: 0,
+//   up() {
+//     this.step++;
+//     return this;
+//   },
+//   down() {
+//     this.step--;
+//     return this;
+//   },
+//   showStep: function () {
+//     // shows the current step
+//     alert(this.step);
+//     return this;
+//   },
+// };
+
+// ladder.up().up().down().showStep().down().showStep();
