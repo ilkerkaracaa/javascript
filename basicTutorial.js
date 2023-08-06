2382b2c7008b42401ec4a81cf6e7d3c55e9de788
İlker Karaca <ikaracadeveloper@gmail.com>
2023-08-06 20:20:59 +0300

    modified:   basicTutorial.js

--- basicTutorial.js
+++ basicTutorial.js
@@ -242,3 +242,37 @@
 // let user = users.find((item) => item.id === 1);
 
 // alert(user.name); // John
+
+// let army = {
+//   minAge: 18,
+//   maxAge: 27,
+//   canJoin(user) {
+//     return user.age >= this.minAge && user.age < this.maxAge;
+//   },
+// };
+
+// let users = [{ age: 16 }, { age: 20 }, { age: 23 }, { age: 30 }];
+
+// // find users, for who army.canJoin returns true
+// let soldiers = users.filter((user) => army.canJoin(user));
+
+// alert(soldiers.length); // 2
+// alert(soldiers[0].age); // 20
+// alert(soldiers[1].age); // 23
+
+function camelize(string) {
+  let result = "";
+  let arr = string.split("-");
+  for (let value of arr) {
+    if (arr[0] === value) {
+      result += value;
+      continue;
+    }
+    result += value.charAt(0).toUpperCase() + value.slice(1);
+  }
+  alert(result);
+}
+
+camelize("background-color");
+camelize("list-style-image");
+camelize("-webkit-transition");
