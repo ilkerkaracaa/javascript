3f05f31d6d6c182732e3ca2b9bd38bd666df3a18
İlker Karaca <ikaracadeveloper@gmail.com>
2023-08-13 22:34:21 +0300

    modified:   basicTutorial.js

--- basicTutorial.js
+++ basicTutorial.js
@@ -654,3 +654,13 @@
 // };
 // console.log(person);
 // console.log(person.getPersonInfo());
+
+// a callback function, the name of the function could be any name
+// const callback = (n) => {
+//   return n ** 2;
+// };
+// // function that takes other function as a callback
+// function cube(callback, n) {
+//   return callback(n) * n;
+// }
+// console.log(cube(callback, 3));
