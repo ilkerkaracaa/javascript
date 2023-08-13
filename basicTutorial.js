d984dcc239b718750a9992e35b2aec68cc996e0c
İlker Karaca <ikaracadeveloper@gmail.com>
2023-08-14 01:01:37 +0300

    modified:   basicTutorial.js

--- basicTutorial.js
+++ basicTutorial.js
@@ -691,3 +691,43 @@
 // let C = new Set(c);
 
 // console.log(C);
+
+// const person = {
+//   firstName: "Asabeneh",
+//   lastName: "Yetayeh",
+//   age: 250,
+//   country: "Finland",
+//   job: "Instructor and Developer",
+//   skills: [
+//     "HTML",
+//     "CSS",
+//     "JavaScript",
+//     "React",
+//     "Redux",
+//     "Node",
+//     "MongoDB",
+//     "Python",
+//     "D3.js",
+//   ],
+//   languages: ["Amharic", "English", "Suomi(Finnish)"],
+// };
+// // Let us create a function which give information about the person object without destructuring
+
+// const getPersonInfo = (obj) => {
+//   const skills = obj.skills;
+//   const formattedSkills = skills.slice(0, -1).join(", ");
+//   const languages = obj.languages;
+//   const formattedLanguages = languages.slice(0, -1).join(", ");
+
+//   personInfo = `${obj.firstName} ${obj.lastName} lives in ${
+//     obj.country
+//   }. He is  ${obj.age} years old. He is an ${
+//     obj.job
+//   }. He teaches ${formattedSkills} and ${
+//     skills[skills.length - 1]
+//   }. He speaks ${formattedLanguages} and a little bit of ${languages[2]}.`;
+
+//   return personInfo;
+// };
+
+// console.log(getPersonInfo(person));
